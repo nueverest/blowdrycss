aec3f4742271d5fd3defda861f4db3251fd87981
Chad Nelson <nu.everest@gmail.com>
2015-11-06 10:51:39 -0700

    Added comment for project skeleton.

---
+++ python/blowdry.py
@@ -0,0 +1,25 @@
+import os
+os.path.dirname(os.path.abspath(__file__))
+
+# Set project directory.
+project_directory = "..\ExampleSite"
+
+# Get all files in project directory
+
+
+# Detect all defined classes
+
+
+# Determine which class names match the format
+
+
+# Generate a DRY CSS file.
+
+
+# Output the DRY CSS file. (user command option)
+
+
+# Minify the DRY CSS file.
+
+
+# Output the Minified DRY CSS file. (user command option)
