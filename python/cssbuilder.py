71e806af1b264414c819019d94a6f6a2a9e43f0e
Chad Nelson <nu.everest@gmail.com>
2015-11-13 07:17:22 -0700

    All tests passing.

--- python/cssbuilder.py
+++ python/cssbuilder.py
@@ -46,6 +46,7 @@ class CSSBuilder(object):
                     invalid_css_classes.append(css_class)
                     reasons.append(' (cssutils invalid property value: ' + value + ')')
                     continue
+            # This exception can't be tested as clean_class_set() and get_property_value() prevent it.
             except SyntaxErr:
                 invalid_css_classes.append(css_class)
                 reasons.append(' (cssutils SyntaxErr invalid property value: ' + value + ')')
--- python/unit_tests/test_CSSBuilder.py
+++ python/unit_tests/test_CSSBuilder.py
@@ -11,17 +11,29 @@ class TestCSSStyleBuilder(TestCase):
         class_set = {
             'margin-top-10', 'bgc-h000', 'hide', 'margin-20', 'padding-top-10', 'height-200', 'padding-10',
             'valign-middle', 'b', 'width-150', 'width-50', 'font-size-48', 'c-blue', 'margin-top-50px',
-            'text-align-center', 'height-50px', 'height-150px', 'bold', 'color-hfff'
+            'text-align-center', 'height-50px', 'height-150px', 'bold', 'color-hfff', 'padding-b1 a5 c1% e5',
+            'margin-1a% 10x% 3q% 1mp3',
         }
         expected_clean_set = {
             'c-blue', 'height-150px', 'bgc-h000', 'bold', 'color-hfff', 'height-50px', 'text-align-center',
-            'margin-top-50px', 'valign-middle'
+            'margin-top-50px', 'valign-middle',
         }
-        expected_removed_set = {'b (property_name not found in self.property_dict.)', 'margin-20 (cssutils invalid property value: 20)', 'padding-10 (cssutils invalid property value: 10)', 'margin-top-10 (cssutils invalid property value: 10)', 'font-size-48 (cssutils invalid property value: 48)', 'width-150 (cssutils invalid property value: 150)', 'padding-top-10 (cssutils invalid property value: 10)', 'hide (property_name not found in self.property_dict.)', 'height-200 (cssutils invalid property value: 200)', 'width-50 (cssutils invalid property value: 50)'}
+        expected_removed_set = {
+            'b (property_name not found in self.property_dict.)', 'margin-20 (cssutils invalid property value: 20)',
+            'padding-10 (cssutils invalid property value: 10)', 'margin-top-10 (cssutils invalid property value: 10)',
+            'font-size-48 (cssutils invalid property value: 48)', 'width-150 (cssutils invalid property value: 150)',
+            'padding-top-10 (cssutils invalid property value: 10)',
+            'hide (property_name not found in self.property_dict.)',
+            'height-200 (cssutils invalid property value: 200)', 'width-50 (cssutils invalid property value: 50)',
+            'padding-b1 a5 c1% e5 (Only a-z, 0-9, "_", and "-" are allowed in class name.)',
+            'margin-1a% 10x% 3q% 1mp3 (Only a-z, 0-9, "_", and "-" are allowed in class name.)',
+        }
         property_parser = ClassPropertyParser(class_set=class_set)
         style_builder = CSSBuilder(property_parser=property_parser)
-        self.assertTrue(style_builder.property_parser.class_set == expected_clean_set, msg=style_builder.property_parser.class_set)
-        self.assertTrue(style_builder.property_parser.removed_class_set == expected_removed_set, msg=style_builder.property_parser.removed_class_set)
+        self.assertTrue(style_builder.property_parser.class_set == expected_clean_set,
+                        msg=style_builder.property_parser.class_set)
+        self.assertTrue(style_builder.property_parser.removed_class_set == expected_removed_set,
+                        msg=style_builder.property_parser.removed_class_set)
 
     def test_get_css_text_output(self):
         class_set = {
@@ -30,16 +42,14 @@ class TestCSSStyleBuilder(TestCase):
             'text-align-center', 'height-50px', 'height-150px', 'bold', 'color-hfff'
         }
         expected_properties = [
-            'background-color: #000;', 'vertical-align: middle;', 'color: blue;', 'margin-top: 50px;',
-            'text-align: center;', 'height: 50px;', 'height: 150px;', 'font-weight: bold;', 'color: #fff;'
+            'background-color: #000', 'vertical-align: middle', 'color: blue', 'margin-top: 50px',
+            'text-align: center', 'height: 50px', 'height: 150px', 'font-weight: bold', 'color: #fff'
         ]
         property_parser = ClassPropertyParser(class_set=class_set)
-        style_builder = CSSBuilder(property_parser=property_parser)
-        css_text = style_builder.build_stylesheet()
+        css_builder = CSSBuilder(property_parser=property_parser)
+        css_text = css_builder.get_css_text().decode('utf-8')
 
         for expected in expected_properties:
-            self.assertTrue(expected in css_text)
+            self.assertTrue(expected in css_text, msg=expected + ' and ' + css_text)
             if expected in css_text:
                 css_text = css_text.replace(expected, '')
-
-        self.assertTrue(css_text.strip() == '', msg=css_text)   # remove whitespace
