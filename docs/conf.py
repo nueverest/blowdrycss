b7b7be07551daef23971866ced3bb0c9628a2dc2
Chad Nelson <nu.everest@gmail.com>
2015-12-12 14:03:51 -0700

    Set html_static_path = [] to avoid the "copying static files... WARNING: html_static_path entry '/home/docs/checkouts/readthedocs.org/readthedocs/templates/sphinx/_static' does not exist" on readthedocs.
    Reference: https://github.com/rtfd/readthedocs.org/issues/1776#issuecomment-149684640

--- docs/conf.py
+++ docs/conf.py
@@ -154,7 +154,10 @@ html_favicon = 'favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+
+# Changed to prevent readthedocs from looking inside the empty _static folder.
+# Reference: https://github.com/rtfd/readthedocs.org/issues/1776#issuecomment-149684640
+html_static_path = []   # ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
