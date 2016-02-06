62b6895f82b657b0265092a53f110c050b94648e
nu.everest <nu.everest@gmail.com>
2016-02-05 20:04:52 -0700

    'python setup.py test', 'python -m unittest ...', and pycharm unittests all pass.

--- blowdrycss/unit_tests/test_write_blowdrycss_settings_dot_py.py
+++ blowdrycss/unit_tests/test_write_blowdrycss_settings_dot_py.py
@@ -20,49 +20,47 @@ class TestWrite_blowdrycss_settings_dot_py(TestCase):
         self.assertTrue(path.isfile('blowdrycss_settings.py'))
 
         # Import from the current folder.
-        try:
-            import blowdrycss_settings as settings                          # development case
-        except ImportError:
-            import blowdrycss.unit_tests.blowdrycss_settings as settings    # python setup.py test
+        import blowdrycss_settings as test_settings    # python setup.py test
 
-        # test directory and file_type settings
+        # test directory settings
         cwd = getcwd()
-        self.assertTrue(
-            settings.markdown_directory == path.join(cwd, 'docs', 'markdown'),
-            msg=settings.markdown_directory + '\t' + path.join(cwd, 'docs', 'markdown')
+        self.assertEqual(
+            test_settings.markdown_directory, path.join(cwd, 'docs', 'markdown'),
+            msg=test_settings.markdown_directory + '\t' + path.join(cwd, 'docs', 'markdown')
         )
-        self.assertTrue(settings.project_directory == cwd)
-        self.assertTrue(settings.css_directory == path.join(cwd, 'css'))
-        self.assertTrue(settings.docs_directory == path.join(cwd, 'docs'))
+        self.assertEqual(test_settings.project_directory, cwd)
+        self.assertEqual(test_settings.css_directory, path.join(cwd, 'css'))
+        self.assertEqual(test_settings.docs_directory, path.join(cwd, 'docs'))
 
-        self.assertTrue(settings.file_types == ('*.html', ))
+        # test file_type settings
+        self.assertTrue(test_settings.file_types == ('*.html', ))
 
         # test accessibility of true settings
         true_settings = [
-            settings.timing_enabled, settings.human_readable, settings.minify, settings.media_queries_enabled,
-            settings.use_em
+            test_settings.timing_enabled, test_settings.human_readable, test_settings.minify, test_settings.media_queries_enabled,
+            test_settings.use_em
         ]
         for true_setting in true_settings:
             self.assertTrue(true_setting)
 
         # test accessibility of false settings
-        false_settings = [settings.markdown_docs, settings.html_docs, settings.rst_docs]
+        false_settings = [test_settings.markdown_docs, test_settings.html_docs, test_settings.rst_docs]
         for false_setting in false_settings:
             self.assertFalse(false_setting)
 
         # test base, px_to_em, and breakpoints
-        self.assertTrue(settings.base == 16)
+        self.assertTrue(test_settings.base == 16)
 
-        self.assertTrue(settings.xxsmall == (settings.px_to_em(0), settings.px_to_em(120)))
-        self.assertTrue(settings.xsmall == (settings.px_to_em(121), settings.px_to_em(240)))
-        self.assertTrue(settings.small == (settings.px_to_em(241), settings.px_to_em(480)))
-        self.assertTrue(settings.medium == (settings.px_to_em(481), settings.px_to_em(720)))
-        self.assertTrue(settings.large == (settings.px_to_em(721), settings.px_to_em(1024)))
-        self.assertTrue(settings.xlarge == (settings.px_to_em(1025), settings.px_to_em(1366)))
-        self.assertTrue(settings.xxlarge == (settings.px_to_em(1367), settings.px_to_em(1920)))
-        self.assertTrue(settings.giant == (settings.px_to_em(1921), settings.px_to_em(2560)))
-        self.assertTrue(settings.xgiant == (settings.px_to_em(2561), settings.px_to_em(2800)))
-        self.assertTrue(settings.xxgiant == (settings.px_to_em(2801), settings.px_to_em(10**6)))
+        self.assertTrue(test_settings.xxsmall == (test_settings.px_to_em(0), test_settings.px_to_em(120)))
+        self.assertTrue(test_settings.xsmall == (test_settings.px_to_em(121), test_settings.px_to_em(240)))
+        self.assertTrue(test_settings.small == (test_settings.px_to_em(241), test_settings.px_to_em(480)))
+        self.assertTrue(test_settings.medium == (test_settings.px_to_em(481), test_settings.px_to_em(720)))
+        self.assertTrue(test_settings.large == (test_settings.px_to_em(721), test_settings.px_to_em(1024)))
+        self.assertTrue(test_settings.xlarge == (test_settings.px_to_em(1025), test_settings.px_to_em(1366)))
+        self.assertTrue(test_settings.xxlarge == (test_settings.px_to_em(1367), test_settings.px_to_em(1920)))
+        self.assertTrue(test_settings.giant == (test_settings.px_to_em(1921), test_settings.px_to_em(2560)))
+        self.assertTrue(test_settings.xgiant == (test_settings.px_to_em(2561), test_settings.px_to_em(2800)))
+        self.assertTrue(test_settings.xxgiant == (test_settings.px_to_em(2801), test_settings.px_to_em(10**6)))
 
         # Clean up by deleting test settings file.  Can be commented to see what the generated file looks like.
         # Remove it if it exists.
--- blowdrycss/utilities.py
+++ blowdrycss/utilities.py
@@ -7,12 +7,9 @@ from inspect import currentframe
 from os import path, stat, getcwd
 # custom
 try:
-    import settings.blowdrycss_settings as settings                     # development case
+    import settings.blowdrycss_settings as settings             # development case
 except ImportError:
-    try:
-        import blowdrycss_settings as settings                          # development "python setup.py test" case
-    except ImportError:
-        import blowdrycss.blowdrycss_settings as settings               # packaged deployment case
+    import blowdrycss.blowdrycss_settings as settings           # packaged deployment case
 
 __author__ = 'chad nelson'
 __project__ = 'blowdrycss'
