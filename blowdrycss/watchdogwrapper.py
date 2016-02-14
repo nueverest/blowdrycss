dadcc23a856733692301df149a0c7905f8727fba
nu.everest <nu.everest@gmail.com>
2016-02-14 13:19:56 -0700

    Integrated settings.auto-generate with watchdogwrapper.

--- blowdrycss/watchdogwrapper.py
+++ blowdrycss/watchdogwrapper.py
@@ -1,46 +1,105 @@
 # builtins
+import logging
 from time import sleep
 # plugins
-from watchdog.events import PatternMatchingEventHandler
+from watchdog.events import PatternMatchingEventHandler, FileModifiedEvent, FileDeletedEvent
 from watchdog.observers import Observer
 # custom
 import blowdrycss.blowdrycss_settings as settings
 from blowdrycss import blowdry
 
 
-import logging
-logging.basicConfig(level=logging.DEBUG)
-
-
 class FileEditEventHandler(PatternMatchingEventHandler):
-    """ Child of PatternMatchingEventHandler
+    """ Child of PatternMatchingEventHandler that only runs blowdry.main() during file 'modified' or 'deleted' events.
+    The 'modified' case handles both the 'created' and 'moved' case. When a file is created or moved/copy/pasted
+    into the project_directory, 'modified' is triggered. This reduces a number of unnecessary calls to blowdry.main().
+
     """
-    def on_any_event(self, event):
-        logging.debug(event)
-        # blowdry.main()
+    @staticmethod
+    def print_status():
+        file_types = '(' + ', '.join(settings.file_types) + ')'
 
-event_handler = FileEditEventHandler(
-        patterns=list(settings.file_types),
-        ignore_patterns=[],
-        ignore_directories=True
-)
+        print()
+        print('-' * 96)
+        print(
+            'blowdry_watchdog is now watching all', file_types, 'files',
+            '\nin the project directory:', settings.project_directory
+        )
+        print('-' * 96)
+        print('Press Ctrl + C to exit.')
+        print()
 
-observer = Observer()
-observer.schedule(event_handler, settings.project_directory, recursive=True)
-observer.start()
+    @staticmethod
+    def excluded(src_path=''):
+        """
+        :param src_path:
+        :return: (*bool*) -- Return True if ``src_path`` ends with a file in ``excluded_files``
 
-print('-' * 96)
-print(
-    'Watchdog is now watching all files of type:', settings.file_types,
-    '\nin the project directory:', settings.project_directory
-)
-print('-' * 96)
-print('Press Ctrl + C to exit.')
+        """
+        excluded_files = ('clashing_aliases.html', 'property_aliases.html')
+        for excluded_file in excluded_files:
+            if src_path.endswith(excluded_file):
+                return True
+        return False
 
-try:
-    while True:
-        sleep(1)
-except KeyboardInterrupt:
-    observer.stop()
+    def on_modified(self, event):
+        """ Called when a file or directory is modified. Only FileModifiedEvents trigger action.
 
-observer.join()
+        :type event: :class:`watchdog.event.DirModifiedEvent` or :class:`watchdog.event.FileModifiedEvent`
+        :param event: Event representing file modification.
+
+        """
+        if type(event) == FileModifiedEvent and not self.excluded(src_path=event.src_path):
+            logging.debug('  ' + event.event_type + ' :: ' + str(event.key))
+            blowdry.main()
+            self.print_status()
+
+    def on_deleted(self, event):
+        """ Called when a file or directory is deleted. Only FileDeletedEvents triggers blowdry.main().
+
+        :type event: :class:`watchdog.event.DirDeletedEvent` or :class:`watchdog.event.FileDeletedEvent`
+        :param event: Event representing file deletion.
+
+        """
+        if type(event) == FileDeletedEvent and not self.excluded(src_path=event.src_path):
+            logging.debug('  ' + event.event_type.ljust(8) + ' :: ' + str(event))
+            blowdry.main()
+            self.print_status()
+
+
+def main():
+    """ Creates and attaches an file event handler to an observer. When a file is modified or deleted it triggers
+    blowdry.main().
+
+    :return: None
+
+    """
+    if settings.auto_generate:
+        # Uncomment for testing
+        # logging.basicConfig(level=logging.DEBUG)
+
+        event_handler = FileEditEventHandler(
+                patterns=list(settings.file_types),
+                ignore_patterns=[],
+                ignore_directories=True
+        )
+
+        observer = Observer()
+        observer.schedule(event_handler, settings.project_directory, recursive=True)
+        observer.start()
+
+        event_handler.print_status()
+
+        try:
+            while True:
+                sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+
+        observer.join()
+    else:
+        blowdry.main()
+
+
+if __name__ == '__main__':
+    main()
