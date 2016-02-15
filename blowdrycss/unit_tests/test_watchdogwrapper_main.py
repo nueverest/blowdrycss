8ed8c6e395377bb40d4abf121e3b8355cd75e99b
nu.everest <nu.everest@gmail.com>
2016-02-15 15:28:58 -0700

    Only handle False case for auto-generate. The true case seems to require creating a special separate process.

--- blowdrycss/unit_tests/test_watchdogwrapper_main.py
+++ blowdrycss/unit_tests/test_watchdogwrapper_main.py
@@ -1,6 +1,5 @@
 # python 2
 from __future__ import print_function, unicode_literals, with_statement
-from builtins import str
 # builtins
 from unittest import TestCase, main
 import logging
@@ -12,12 +11,60 @@ from os import path, remove
 from blowdrycss.utilities import unittest_file_path, change_settings_for_testing
 from blowdrycss import watchdogwrapper
 
+# required for pycharm unittest feature to work under both python 2.7 and python 3.x
+if sys.hexversion < 0x03000000:
+    import blowdrycss.blowdrycss_settings as settings
+else:
+    import blowdrycss_settings as settings
 
 change_settings_for_testing()
 
 
 class TestWatchdogWrapperMain(TestCase):
-    def test_main(self):
+    # Unable to test this at present as main() is in a process of its' own.
+    # Need something like this http://stackoverflow.com/questions/7602120/sending-keyboard-interrupt-programmatically
+
+    # def test_main_auto_generate_True(self):
+    #     # Integration test
+    #     logging.basicConfig(level=logging.DEBUG)
+    #     substrings = [
+    #         '~~~ blowdrycss started ~~~',
+    #         'File Types: *.html',
+    #         'Project Directory:',
+    #         'Auto-Generated CSS',
+    #         'Completed',
+    #         'blowdry.css',
+    #         'blowdry.min.css',
+    #     ]
+    #     html_text = '<html></html>'
+    #     delete_dot_html = unittest_file_path(folder='test_examplesite', filename='delete.html')
+    #
+    #     # Create delete.html
+    #     with open(delete_dot_html, 'w') as _file:
+    #         _file.write(html_text)
+    #
+    #     self.assertTrue(path.isfile(delete_dot_html))
+    #
+    #     saved_stdout = sys.stdout
+    #     try:
+    #         out = StringIO()
+    #         sys.stdout = out
+    #
+    #         settings.auto_generate = True
+    #         watchdogwrapper.main()
+    #
+    #         remove(delete_dot_html)     # Delete delete.html
+    #
+    #         sleep(0.25)                 # IMPORTANT: Must wait for output otherwise test will fail.
+    #
+    #         output = out.getvalue()
+    #
+    #         for substring in substrings:
+    #             self.assertTrue(substring in output, msg=substring + '\noutput:\n' + output)
+    #     finally:
+    #         sys.stdout = saved_stdout
+
+    def test_main_auto_generate_False(self):
         # Integration test
         logging.basicConfig(level=logging.DEBUG)
         substrings = [
@@ -43,6 +90,7 @@ class TestWatchdogWrapperMain(TestCase):
             out = StringIO()
             sys.stdout = out
 
+            settings.auto_generate = False
             watchdogwrapper.main()
 
             remove(delete_dot_html)     # Delete delete.html
