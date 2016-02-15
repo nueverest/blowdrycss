1c972da5898fde2298b51d86b4232ef1ddfaf9da
nu.everest <nu.everest@gmail.com>
2016-02-14 17:30:42 -0700

    Fixed failing timer tests and added new ones.

--- blowdrycss/unit_tests/test_timing.py
+++ blowdrycss/unit_tests/test_timing.py
@@ -6,7 +6,7 @@ from string import digits
 from io import StringIO
 
 # custom
-import blowdrycss.timing as timing
+from blowdrycss.timing import Timer
 
 __author__ = 'chad nelson'
 __project__ = 'blowdrycss'
@@ -15,14 +15,27 @@ __project__ = 'blowdrycss'
 class TestTiming(TestCase):
     def test_seconds_to_string(self):
         allowed = set(digits + '.')
-        time_string = timing.seconds_to_string(elapsed=time())
+        timer = Timer()
+        time_string = timer.seconds_to_string(seconds_elapsed=time())
         self.assertTrue(set(time_string) <= allowed)
 
-    def test_log(self):
+    def test_elapsed_end_set(self):
+        allowed = set(digits + '.')
+        timer = Timer()
+        timer.end = time()
+        self.assertTrue(set(timer.elapsed) <= allowed)
+
+    def test_elapsed_end_not_set(self):
+        allowed = set(digits + '.')
+        timer = Timer()
+        self.assertTrue(set(timer.elapsed) <= allowed)
+
+    def test_print_time(self):
+        timer = Timer()
+
         # Checks variable date components and constant string output.
-        time_string = timing.seconds_to_string(elapsed=time())
         substrings = [
-            'Completed @', 'It took:', 'seconds', '=====', '.', str(datetime.now().year), str(datetime.now().month),
+            'Completed ', 'It took:', 'seconds', '=====', '.', str(datetime.now().year), str(datetime.now().month),
             str(datetime.now().day)
         ]
         saved_stdout = sys.stdout
@@ -30,7 +43,7 @@ class TestTiming(TestCase):
             out = StringIO()
             sys.stdout = out
 
-            timing.log(elapsed=time_string)
+            timer.print_time()
 
             output = out.getvalue()
             for substring in substrings:
@@ -38,10 +51,12 @@ class TestTiming(TestCase):
         finally:
             sys.stdout = saved_stdout
 
-    def test_end_log(self):
+    def test_report(self):
+        timer = Timer()
+
         # Checks variable date components and constant string output.
         substrings = [
-            'Completed @', 'It took:', 'seconds', '=====', '.', str(datetime.now().year), str(datetime.now().month),
+            'Completed ', 'It took:', 'seconds', '=====', '.', str(datetime.now().year), str(datetime.now().month),
             str(datetime.now().day)
         ]
         saved_stdout = sys.stdout
@@ -49,7 +64,7 @@ class TestTiming(TestCase):
             out = StringIO()
             sys.stdout = out
 
-            timing.end_log()
+            timer.report()
 
             output = out.getvalue()
             for substring in substrings:
