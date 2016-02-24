b0be9ddf400c3b8ba0e260e32b4a0c781bb30dd7
nu.everest <nu.everest@gmail.com>
2016-02-23 18:44:50 -0700

    Added settings tests for logging and custom_property_alias_dict.

--- blowdrycss/unit_tests/test_write_blowdrycss_settings_dot_py.py
+++ blowdrycss/unit_tests/test_write_blowdrycss_settings_dot_py.py
@@ -1,6 +1,7 @@
 # builtins
 from unittest import TestCase, main
 from os import getcwd, path, remove, chdir
+from logging import DEBUG
 # custom
 from blowdrycss.settingsbuilder import write_blowdrycss_settings_dot_py
 
@@ -42,6 +43,17 @@ class TestWrite_blowdrycss_settings_dot_py(TestCase):
         self.assertEqual(test_settings.css_directory, path.join(cwd, 'examplesite', 'css'))
         self.assertEqual(test_settings.docs_directory, path.join(cwd, 'docs'))
 
+        # Logging
+        self.assertEqual(test_settings.logging_enabled, False)
+        self.assertEqual(test_settings.logging_level, DEBUG)
+        self.assertEqual(test_settings.log_to_console, False)
+        self.assertEqual(test_settings.log_to_file, False)
+        self.assertEqual(test_settings.log_directory, path.join(cwd, 'log'))
+        self.assertEqual(test_settings.log_file_name, 'blowdrycss.log')
+        one_mega_byte = 1048576
+        self.assertEqual(test_settings.log_file_size, 4 * one_mega_byte)
+        self.assertEqual(test_settings.log_backup_count, 1)
+
         # test file_type settings
         self.assertTrue(test_settings.file_types == ('*.html', ))
 
@@ -72,6 +84,28 @@ class TestWrite_blowdrycss_settings_dot_py(TestCase):
         self.assertTrue(test_settings.giant == (test_settings.px_to_em(1921), test_settings.px_to_em(2560)))
         self.assertTrue(test_settings.xgiant == (test_settings.px_to_em(2561), test_settings.px_to_em(2800)))
         self.assertTrue(test_settings.xxgiant == (test_settings.px_to_em(2801), test_settings.px_to_em(10**6)))
+
+        # custom_property_alias_dict
+        self.assertEqual(
+            test_settings.custom_property_alias_dict,
+            {
+                'background': {'bg-', },
+                'background-color': {'bgc-', 'bg-c-', 'bg-color-', },
+                'color': {'c-', },
+                'font-size': {'fsize-', 'f-size-', },
+                'font-weight': {'fweight-', 'f-weight-', },
+                'height': {'h-', },
+                'margin': {'m-', },
+                'margin-top': {'m-top-', },
+                'margin-bottom': {'m-bot-', },
+                'padding': {'p-', 'pad-', },
+                'padding-top': {'p-top-', },
+                'position': {'pos-', },
+                'text-align': {'talign-', 't-align-', },
+                'vertical-align': {'valign-', 'v-align-', },
+                'width': {'w-', },
+            }
+        )
 
         # Clean up. Removed created file.
         if path.isfile(settings_file):                          # Remove it if it exists.
