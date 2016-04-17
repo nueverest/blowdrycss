4a5a57d46ecbafc1a67a766291a09052aeced01e
nu.everest <nu.everest@gmail.com>
2016-04-16 20:52:48 -0700

    Manually create blowdry.css and .min.css for both local and Travis testing.

--- blowdrycss/unit_tests/test_utilities.py
+++ blowdrycss/unit_tests/test_utilities.py
@@ -171,38 +171,32 @@ class Test_utilities(TestCase):
                 self.assertTrue(else_case)
 
     def test_print_css_stats(self):
-        substrings = [
-            'blowdry.css:\t 0.3kB',
-            'blowdry.min.css: 0.2kB',
-            'CSS file size reduced by 67.5%.'
-        ]
-
         # On Travis CI these auto-generated files are inaccessible and need to be recreated.
         # Change the expected file size reduction percentage since Ubuntu's math is different.
         # Create directories and CSS files if they do not exist.
         blowdry_css = unittest_file_path('test_examplesite/test_css', 'blowdry.css')
         blowdry_min_css = unittest_file_path('test_examplesite/test_css', 'blowdry.min.css')
 
-        if not path.isfile(blowdry_css) or not path.isfile(blowdry_min_css):
-            substrings = [
-                'blowdry.css:\t 0.3kB',
-                'blowdry.min.css: 0.2kB',
-                'CSS file size reduced by 71.6%.'
-            ]
+        #if not path.isfile(blowdry_css) or not path.isfile(blowdry_min_css):
+        substrings = [
+            'blowdry.css:\t 0.3kB',
+            'blowdry.min.css: 0.2kB',
+            'CSS file size reduced by 71.6%.'
+        ]
 
-            blowdry_css_text = '.bgc-hf8f8f8 {\n    background-color: #f8f8f8\n    }\n.border-1px-solid-gray {\n    border: 1px solid gray\n    }\n.padding-5 {\n    padding: 0.3125em\n    }\n.bold {\n    font-weight: bold\n    }\n.talign-center {\n    text-align: center\n    }\n.display-inline {\n    display: inline\n    }'
-            blowdry_min_css_text = '.bgc-hf8f8f8{background-color:#f8f8f8}.border-1px-solid-gray{border:1px solid gray}.padding-5{padding:.3125em}.bold{font-weight:bold}.talign-center{text-align:center}.display-inline{display:inline}'
+        blowdry_css_text = '.bgc-hf8f8f8 {\n    background-color: #f8f8f8\n    }\n.border-1px-solid-gray {\n    border: 1px solid gray\n    }\n.padding-5 {\n    padding: 0.3125em\n    }\n.bold {\n    font-weight: bold\n    }\n.talign-center {\n    text-align: center\n    }\n.display-inline {\n    display: inline\n    }'
+        blowdry_min_css_text = '.bgc-hf8f8f8{background-color:#f8f8f8}.border-1px-solid-gray{border:1px solid gray}.padding-5{padding:.3125em}.bold{font-weight:bold}.talign-center{text-align:center}.display-inline{display:inline}'
 
-            # Create directories.
-            make_directory(unittest_file_path('test_examplesite', ''))
-            make_directory(unittest_file_path('test_examplesite/test_css', ''))
+        # Create directories.
+        make_directory(unittest_file_path('test_examplesite', ''))
+        make_directory(unittest_file_path('test_examplesite/test_css', ''))
 
-            # Create files.
-            with open(blowdry_css, 'wb') as generic_file:
-                generic_file.write(bytearray(blowdry_css_text, 'utf-8'))
+        # Create files.
+        with open(blowdry_css, 'wb') as generic_file:
+            generic_file.write(bytearray(blowdry_css_text, 'utf-8'))
 
-            with open(blowdry_min_css, 'wb') as generic_file:
-                generic_file.write(bytearray(blowdry_min_css_text, 'utf-8'))
+        with open(blowdry_min_css, 'wb') as generic_file:
+            generic_file.write(bytearray(blowdry_min_css_text, 'utf-8'))
 
         # Handle printed output.
         saved_stdout = sys.stdout
